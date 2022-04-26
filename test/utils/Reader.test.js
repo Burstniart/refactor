0f7fa5befe6fcb60d1afeb6e15f7c3558056486d
Burstniart <v4gr33n@hotmail.com>
2022-04-25 23:52:15 -0500

    Passed test for static readJSon method

--- test/utils/Reader.test.js
+++ test/utils/Reader.test.js
@@ -1,7 +1,15 @@
+const Reader = require('./../../lib/utils/Reader')
+
 describe('Reader class tester', () => {
     test('1) Checking for correct Json read', () => {
-        const res = 1 + 2 
-        
-        expect(res).toBe(3)
+        const explorersList = Reader.readJsonFile('./explorers.json')
+        const explorer0 = {
+            name: 'Woopa1',
+            githubUsername: 'ajolonauta1',
+            score: 1,
+            mission: 'node',
+            stacks: [ 'javascript', 'reasonML', 'elm' ]
+          }
+        expect(explorersList[0]).toStrictEqual(explorer0)
     });
   })
