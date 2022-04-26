7a6b93e6f8acf407facb37bcfc8c73420c92ec0c
Burstniart <v4gr33n@hotmail.com>
2022-04-26 14:14:22 -0500

    Add FizzbuzzService file and test

---
+++ test/services/FizzbuzzService.test.js
@@ -0,0 +1,8 @@
+const FizzbuzzService = require('./../../lib/services/FizzbuzzService')
+
+describe('Test suit for Fizz Buzz', () => {
+    test('1) Fizz on score divisible by 3', () => {
+        const res = 1 + 2
+        expect(res).toBe(53)
+    })
+})
