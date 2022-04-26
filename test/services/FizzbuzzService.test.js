5849dfe298f42a580edeb01f1a227846856703c5
Burstniart <v4gr33n@hotmail.com>
2022-04-26 14:41:52 -0500

    Add Buzz test

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -10,12 +10,19 @@ describe('Test suit for Fizz Buzz', () => {
         expect(explorer1.trick).toBe(1)
         expect(explorer3.trick).toBe('FIZZ')
 
-        // const explorer5 = {name: "Explorer5", score: 5}
-        // // {name: "Explorer5", score: 5, trick: "BUZZ"}
-        // FizzbuzzService.applyValidationInExplorer(explorer5) 
-
+        
         // const explorer15 = {name: "Explorer15", score: 15}
         // // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
         // FizzbuzzService.applyValidationInExplorer(explorer15) 
+    })
+    test('2) Buzz on score divisible by 5', () => {
+        const explorer1 = {name: "Explorer1", score: 1}
+        FizzbuzzService.applyValidationInExplorer(explorer1)
+        const explorer5 = {name: "Explorer5", score: 5}
+        FizzbuzzService.applyValidationInExplorer(explorer5) 
+
+        expect(explorer1.trick).toBe(1)
+        expect(explorer5.trick).toBe('BUZZ')
+
     })
 })
