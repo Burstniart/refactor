8aa44ad867c1136a40c0be51d84b9e3f1969483a
Burstniart <v4gr33n@hotmail.com>
2022-04-26 13:10:59 -0500

    Add filterByMission code and test

--- test/services/ExplorerServices.test.js
+++ test/services/ExplorerServices.test.js
@@ -1,9 +1,12 @@
 const ExplorerService = require('./../../lib/services/ExplorerService')
+const Reader = require('./../../lib/utils/Reader')
 
 describe('Test suit for ExplorerService', () => {
     test('Filter explorer objects by mission', () => {
         // Aplicación del ExplorerService sobre la lista de explorers
-        const noders = ExplorerService.filterByMission('./explorers.json', "node");
+        const explorers = Reader.readJsonFile('explorers.json')
+        const noders = ExplorerService.filterByMission(explorers, "node");
+
         // ExplorerService.getAmountOfExplorersByMission(explorers, "node");
         // ExplorerService.getExplorersUsernamesByMission(explorers, "node");
         expect(noders[0].mission).toBe('node');
