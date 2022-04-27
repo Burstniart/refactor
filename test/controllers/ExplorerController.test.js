c33d758219376dcbd6fc5c5ca2a144b92847779e
Burstniart <v4gr33n@hotmail.com>
2022-04-27 12:16:28 -0500

    Add get amount by mission method

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -6,8 +6,13 @@ describe('Test suit for ExplorerController', () => {
         const explorersInNode = ExplorerController.getExplorersByMission("node")
         expect(explorersInNode[0].mission).toBe("node")
     });
-    test("2) Test for getExplorersAmountByMission method", () => {
+    test("2) Test for getExplorersUsernamesByMission method", () => {
         const explorersInNode = ExplorerController.getExplorersUsernamesByMission("node")
         expect(explorersInNode[0]).toBe("ajolonauta1")
+    });
+    test("3) Test for getExplorersAmountByMission method", () => {
+        const explorersInNode = ExplorerController.getExplorersAmountByMission("node")
+        console.log(explorersInNode);
+        expect(explorersInNode).toBe(10)
     })
 })
