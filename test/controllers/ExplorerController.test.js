92e56ae6ce292456661d44d14cfbb3bcf2cda0c5
Burstniart <v4gr33n@hotmail.com>
2022-04-27 12:11:29 -0500

    Add get usernames by mission method code

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -4,12 +4,10 @@ const ExplorerController = require("./../../lib/controllers/ExplorerController")
 describe('Test suit for ExplorerController', () => {
     test('1) Test for getExplorersByMission method', () => {
         const explorersInNode = ExplorerController.getExplorersByMission("node")
-        console.log(explorersInNode[0]);
         expect(explorersInNode[0].mission).toBe("node")
     });
     test("2) Test for getExplorersAmountByMission method", () => {
         const explorersInNode = ExplorerController.getExplorersUsernamesByMission("node")
-        console.log(explorersInNode);
         expect(explorersInNode[0]).toBe("ajolonauta1")
     })
 })
