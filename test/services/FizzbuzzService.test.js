eed047daaf64966c78e8dc4c8fbdf7febd07c499
Burstniart <v4gr33n@hotmail.com>
2022-04-26 21:55:45 -0500

    Add test for FIZZBUZZ (score divisible by 3 & 5)

--- test/services/FizzbuzzService.test.js
+++ test/services/FizzbuzzService.test.js
@@ -11,18 +11,20 @@ describe('Test suit for Fizz Buzz', () => {
         expect(explorer3.trick).toBe('FIZZ')
 
         
-        // const explorer15 = {name: "Explorer15", score: 15}
-        // // {name: "Explorer15", score: 15, trick: "FIZZBUZZ"}
-        // FizzbuzzService.applyValidationInExplorer(explorer15) 
     })
     test('2) Buzz on score divisible by 5', () => {
         const explorer1 = {name: "Explorer1", score: 1}
         FizzbuzzService.applyValidationInExplorer(explorer1)
         const explorer5 = {name: "Explorer5", score: 5}
         FizzbuzzService.applyValidationInExplorer(explorer5) 
-
+        
         expect(explorer1.trick).toBe(1)
         expect(explorer5.trick).toBe('BUZZ')
-
+    })
+    test('3) FizzBuzz on score divisible by 3 and 5', () => {
+        const explorer15 = {name: "Explorer15", score: 15}
+        FizzbuzzService.applyValidationInExplorer(explorer15) 
+        
+        expect(explorer15.trick).toBe('FIZZBUZZ')
     })
 })
