4a1a2a3ce3ee6a721a14bf373e978e8814ce514b
Burstniart <v4gr33n@hotmail.com>
2022-04-27 12:31:48 -0500

    Eslint fix

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,18 +1,17 @@
-const { getExplorersUsernamesByMission } = require("../../lib/services/ExplorerService");
 const ExplorerController = require("./../../lib/controllers/ExplorerController");
 
-describe('Test suit for ExplorerController', () => {
-    test('1) Test for getExplorersByMission method', () => {
-        const explorersInNode = ExplorerController.getExplorersByMission("node")
-        expect(explorersInNode[0].mission).toBe("node")
+describe("Test suit for ExplorerController", () => {
+    test("1) Test for getExplorersByMission method", () => {
+        const explorersInNode = ExplorerController.getExplorersByMission("node");
+        expect(explorersInNode[0].mission).toBe("node");
     });
     test("2) Test for getExplorersUsernamesByMission method", () => {
-        const explorersInNode = ExplorerController.getExplorersUsernamesByMission("node")
-        expect(explorersInNode[0]).toBe("ajolonauta1")
+        const explorersInNode = ExplorerController.getExplorersUsernamesByMission("node");
+        expect(explorersInNode[0]).toBe("ajolonauta1");
     });
     test("3) Test for getExplorersAmountByMission method", () => {
-        const explorersInNode = ExplorerController.getExplorersAmountByMission("node")
+        const explorersInNode = ExplorerController.getExplorersAmountByMission("node");
         console.log(explorersInNode);
-        expect(explorersInNode).toBe(10)
-    })
-})
+        expect(explorersInNode).toBe(10);
+    });
+});
